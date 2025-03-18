040e8d9f8dc4072d1cf5b8da7d3194ae5c161a61
maldikurniawan <akurniawan2102@gmail.com>
2025-03-18 11:15:13 +0700

    fix time

--- src/pages/JadwalSalat.jsx
+++ src/pages/JadwalSalat.jsx
@@ -6,6 +6,7 @@ import { Sidebar } from "@/components";
 import { OptionCities } from "@/constants/optionCities";
 import moment from "moment";
 import "moment/locale/id";
+import { FaCheck, FaTimes } from "react-icons/fa";
 
 const VITE_API_TIME = import.meta.env.VITE_API_TIME;
 
@@ -37,14 +38,31 @@ const JadwalSalat = () => {
         fetchPrayerTimes();
     }, [selectedCity]);
 
+    const isTimePassed = (time) => {
+        const now = moment();
+        const prayerTime = moment(time, "HH:mm"); // Konversi waktu string ke moment object
+        return now.isAfter(prayerTime); // Cek apakah waktu sudah lewat
+    };
+
+    const prayerList = [
+        { label: "Imsak", time: prayerTimes?.Imsak },
+        { label: "Subuh", time: prayerTimes?.Fajr },
+        { label: "Terbit", time: prayerTimes?.Sunrise },
+        { label: "Dzuhur", time: prayerTimes?.Dhuhr },
+        { label: "Ashar", time: prayerTimes?.Asr },
+        { label: "Maghrib", time: prayerTimes?.Maghrib },
+        { label: "Isya", time: prayerTimes?.Isha },
+    ];
+
     return (
         <AppContext.Provider value={data}>
             <HeaderSalat />
             <Sidebar />
             <BannerJadwal selectedCity={selectedCity} prayerTimes={prayerTimes} />
             <div className="pt-4">
-                <h1 className="text-lg font-bold mb-4 text-center text-[#16a34a] ">
-                    {moment().locale("id").format("dddd, D MMMM YYYY")}
+                <h1 className="mb-4 text-center text-[#16a34a]">
+                    <p className="text-lg font-bold">{moment().locale("id").format("dddd, D MMMM YYYY")}</p>
+                    <p className="text-sm">Kementerian Agama Republik Indonesia</p>
                 </h1>
                 {/* Searchable Select */}
                 <Select
@@ -52,33 +70,25 @@ const JadwalSalat = () => {
                     options={OptionCities}
                     value={selectedCity}
                     onChange={(selectedOption) => setSelectedCity(selectedOption)}
-                    isSearchable={true} // Aktifkan pencarian
+                    isSearchable={true}
                 />
 
                 {/* Prayer Times Display */}
                 {prayerTimes ? (
                     <div className="mt-4 flex flex-col gap-2">
-                        <div className="flex justify-between bg-[#16A34A20] p-2 rounded-md">
-                            <span>Imsak</span> <span>{prayerTimes.Imsak}</span>
-                        </div>
-                        <div className="flex justify-between bg-[#16A34A20] p-2 rounded-md">
-                            <span>Subuh</span> <span>{prayerTimes.Fajr}</span>
-                        </div>
-                        <div className="flex justify-between bg-[#16A34A20] p-2 rounded-md">
-                            <span>Terbit</span> <span>{prayerTimes.Sunrise}</span>
-                        </div>
-                        <div className="flex justify-between bg-[#16A34A20] p-2 rounded-md">
-                            <span>Dzuhur</span> <span>{prayerTimes.Dhuhr}</span>
-                        </div>
-                        <div className="flex justify-between bg-[#16A34A20] p-2 rounded-md">
-                            <span>Ashar</span> <span>{prayerTimes.Asr}</span>
-                        </div>
-                        <div className="flex justify-between bg-[#16A34A20] p-2 rounded-md">
-                            <span>Maghrib</span> <span>{prayerTimes.Maghrib}</span>
-                        </div>
-                        <div className="flex justify-between bg-[#16A34A20] p-2 rounded-md">
-                            <span>Isya</span> <span>{prayerTimes.Isha}</span>
-                        </div>
+                        {prayerList.map(({ label, time }) => (
+                            <div key={label} className="flex justify-between bg-[#16A34A20] p-2 rounded-md">
+                                <span>{label}</span>
+                                <span className="flex items-center gap-2">
+                                    <div>{time}</div>
+                                    {isTimePassed(time) ? (
+                                        <FaCheck className="border border-green-500 rounded-full text-green-500 p-1 w-5 h-5" />
+                                    ) : (
+                                        <FaTimes className="border border-red-500 rounded-full text-red-500 p-1 w-5 h-5" />
+                                    )}
+                                </span>
+                            </div>
+                        ))}
                     </div>
                 ) : (
                     <p>Loading...</p>
